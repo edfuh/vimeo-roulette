778e4d499f9f044f4fc3b0840f8233df9ea34e37
Ed Fuhrken <ed@edfuh.com>
2011-09-21 18:19:58 -0700

    **** spin.com

--- js/app.js
+++ js/app.js
@@ -1,38 +1,50 @@
 (function ($, win) {
     var rId = /\{\{id\}\}/,
+        spinner,
         app = win.app = {
-        currId : 0,
-        defaults : {
-            playerUrl : 'http://player.vimeo.com/video/{{id}}?title=0&byline=0&portrait=0&autoplay=1',
-            apiUrl    : 'http://vimeo.com/api/v2/video/{{id}}.json'
-        },
-        generateId : function () {
-            return app.currId = ~~(Math.random() * 3e7);
-        },
-        init : function () {
-            app.loadEvents();
-        },
-        loadEvents : function () {
-            $('button').bind('click', function () {
-                $.when(app.spinIt()).then(function () {
-                    console.log(arguments);
-                    $('#frame').prop('src', app.defaults.playerUrl.replace(rId, app.currId));
-                }, function () {
-                    console.log(arguments);
+            cache : {},
+            currId : 0,
+            defaults : {
+                playerUrl : 'http://player.vimeo.com/video/{{id}}?title=0&byline=0&portrait=0&autoplay=1',
+                apiUrl    : 'http://vimeo.com/api/v2/video/{{id}}.json'
+            },
+            generateId : function () {
+                var id = ~~(Math.random() * 3e7);
+                app.cache[id] = id;
+                return app.currId = id;
+            },
+            init : function () {
+                app.loadEvents();
+                $.getScript('https://raw.github.com/fgnass/spin.js/master/spin.js', function () {
+                    spinner = new Spinner();
                 });
-            });
-            
-            //TODO
-            $(win).bind('beforeunload', function () {
-                $('#frame').prop('src', 'javascript: void(0)');
-            });
-        }
+            },
+            loadEvents : function () {
+                $('button').bind('click', app._fetch);
+
+                //TODO
+                $(win).bind('beforeunload', function () {
+                    $('#frame').prop('src', 'javascript: void(0)');
+                });
+            }
+        };
+
+    app._fetch = function () {
+        $.when(app.spinIt()).then(function () {
+            console.log(arguments);
+            spinner.stop();
+            $('#frame').prop('src', app.defaults.playerUrl.replace(rId, app.currId));
+        }, function () {
+            setTimeout(app._fetch, 50);
+            console.log('fail');
+        });
     };
 
     app.spinIt = function () {
         app.generateId();
 
         $('#div').text(app.currId);
+        spinner.spin(document.body);
 
         var dfd = $.Deferred();
 
